c2720958a6a60da651d9550e0f0a0473c268ea5b
Dimitri BISCHOFF <dbischof@e3r10p23.42.fr>
2017-06-01 10:32:48 +0200

    s

--- vm/loadmemory.c
+++ vm/loadmemory.c
@@ -6,7 +6,7 @@
 /*   By: dbischof <dbischof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/31 16:53:41 by dbischof          #+#    #+#             */
-/*   Updated: 2017/06/01 10:25:16 by dbischof         ###   ########.fr       */
+/*   Updated: 2017/06/01 10:28:18 by dbischof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int	loadmemory(t_battle *b)
 	space = (MEM_SIZE - total) / b->bots.nb;
 	i = -1;
 	while (++i < b->bots.nb)
-		ft_memcpy(b->memory + (i * space))
+		set_memory(b, (0), b->bots.tab[i]->instructions,
+			b->bots.tab[i]->nb_instructions);
 	return (space);
 }
