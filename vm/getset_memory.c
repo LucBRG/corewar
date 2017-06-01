d120de49c92b7e66445a37275c428cdc0e6ebe07
Dimitri BISCHOFF <dbischof@e3r10p23.42.fr>
2017-06-01 10:25:44 +0200

    getset

--- vm/getset_memory.c
+++ vm/getset_memory.c
@@ -1,30 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   loadmemory.c                                       :+:      :+:    :+:   */
+/*   getset_memory.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbischof <dbischof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/05/31 16:53:41 by dbischof          #+#    #+#             */
-/*   Updated: 2017/06/01 10:25:16 by dbischof         ###   ########.fr       */
+/*   Created: 2017/06/01 10:24:55 by dbischof          #+#    #+#             */
+/*   Updated: 2017/06/01 10:25:18 by dbischof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-int	loadmemory(t_battle *b)
+int		setmemory(t_battle *b, int index, char *s, int len)
 {
-	int	i;
-	int	total;
-	int space;
+	int i;
 
 	i = -1;
-	total = 0;
-	while (++i < b->bots.nb)
-		total += b->bots.tab[i]->nb_instructions;
-	space = (MEM_SIZE - total) / b->bots.nb;
+	if (!b || !s || len <= 0 || MEM_SIZE <= 0)
+		return (0);
+	while (++i < len)
+		b->memory[(index + i) % MEM_SIZE] = s[i];
+	return (1);
+}
+
+char	*getmemory(t_battle *b, int index, char *buff, int len)
+{
+	int i;
+
 	i = -1;
-	while (++i < b->bots.nb)
-		ft_memcpy(b->memory + (i * space))
-	return (space);
+	if (!b || !buff || len <= 0 || MEM_SIZE <= 0)
+		return (NULL);
+	while (++i < len)
+		buff[i] = b->memory[(index + i) % MEM_SIZE];
+	return (buff);
 }
