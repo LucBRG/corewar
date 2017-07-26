0141731774c39d8852d8d59b0f3f4cfae50f2a36
Tony FERRARI <tferrari@e3r6p5.42.fr>
2017-07-26 19:08:20 +0200

    norme, -dump, sound

--- vm/src/main.c
+++ vm/src/main.c
@@ -1,12 +1,7 @@
 
 #include "vm.h"
 
-<<<<<<< HEAD
-#define ISFLAG(l)	(!!ft_strchr(av[i], l))
-#define OPTS		battle->opts
-=======
 #define IFFLAG(l)		(!!ft_strstr(av[i], l))
->>>>>>> vm_tferrari
 
 void	initoptions(int ac, char **av, t_battle *battle)
 {
@@ -16,17 +11,11 @@ void	initoptions(int ac, char **av, t_battle *battle)
 	while (++i < ac)
 		if (av[i][0] == '-')
 		{
-<<<<<<< HEAD
-			OPTS.ncurses = (!OPTS.ncurses) ? ISFLAG('n') : OPTS.ncurses;
-			if (ISFLAG('v') && i + 1 < ac)
-				battle->opts.verbose = ft_atoi(av[i + 1]);
-=======
 			OPTS.ncurses = (!OPTS.ncurses) ? IFFLAG("n") : OPTS.ncurses;
 			if (IFFLAG("v") && i + 1 < ac)
 				OPTS.verbose = ft_atoi(av[i + 1]);
 			if ((OPTS.dump = IFFLAG("dump")) && i + 1 < ac)
 				OPTS.n_dump = ft_atoi(av[i + 1]);
->>>>>>> vm_tferrari
 		}
 }
 
